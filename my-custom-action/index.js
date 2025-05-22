3b66bd7f926b314a3973ad31a8a593b2cab9aedf
ntnaz <connectnaz1984n@gmail.com>
2025-05-22 14:33:06 -0500

    Create index.js
    added index.js file

---
+++ my-custom-action/index.js
@@ -0,0 +1,8 @@
+const core = require('@actions/core');
+
+try {
+  const name = core.getInput('name');
+  console.log(`👋 Hello, ${name}!`);
+} catch (error) {
+  core.setFailed(`Action failed with error: ${error}`);
+}
